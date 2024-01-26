9eff4eddf58e78205c298434e4d26191b052e19c
Nat Poulson <nat.poulson@outlook.com.au>
2024-01-26 11:38:43 +1100

    Create placeholder question objects

--- index.js
+++ index.js
@@ -3,7 +3,35 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: "title"
+    },
+    {
+        name: "description"
+    },
+    {
+        name: 'installation'
+    },
+    {
+        name: 'usage'
+    },
+    {
+        name: 'contributing'
+    },
+    {
+        name: 'tests'
+    },
+    {
+        name: 'questions'
+    },
+    {
+        name: 'github'
+    },
+    {
+        name: 'email'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
