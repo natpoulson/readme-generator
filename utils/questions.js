04734d550cf27b879d39cd4effd741899bcfe1d4
Nat Poulson <nat.poulson@outlook.com.au>
2024-01-29 00:18:52 +1100

    Reorder question statics to match flow

--- utils/questions.js
+++ utils/questions.js
@@ -3,7 +3,59 @@ const inquirer = require("inquirer");
 class Questions {
     static message = {
         welcome: `Welcome to the README Generator.\nThis app will pose you a series of questions to help customise and create a professional README file.\nYou'll be asked a series of questions to customise the contents, and then given the option to edit the generated README.`
-    }
+    };
+
+    static required = [
+        {
+            name: "description.title",
+            message: `What is the name of your app?\n\t`,
+            type: 'input'
+        },
+        {
+            name: "description.summary",
+            message: `What does your app do?\n\t`,
+            type: 'input'
+        },
+        {
+            name: "license",
+            message: `What license will you be attributing to this project?`,
+            type: 'list',
+            choices: [
+                {
+                    name: 'GNU AGPL v3',
+                    value: 'agpl3'
+                },
+                {
+                    name: 'GNU GPL v3',
+                    value: 'gpl3'
+                },
+                {
+                    name: 'GNU LGPL v3',
+                    value: 'lgpl3'
+                },
+                {
+                    name: 'Mozilla Public License 2.0',
+                    value: 'mpl2'
+                },
+                {
+                    name: 'Apache License 2.0',
+                    value: 'al2'
+                },
+                {
+                    name: 'MIT License',
+                    value: 'mit'
+                },
+                {
+                    name: 'Boost Software License 1.0',
+                    value: 'boost1'
+                },
+                {
+                    name: 'The Unlicense',
+                    value: 'unlicense'
+                }
+            ]
+        }
+    ];
 
     static sections = {
         name: "sections",
@@ -86,58 +138,6 @@ class Questions {
         ]
     };
 
-    static required = [
-        {
-            name: "description.title",
-            message: `What is the name of your app?\n\t`,
-            type: 'input'
-        },
-        {
-            name: "description.summary",
-            message: `What does your app do?\n\t`,
-            type: 'input'
-        },
-        {
-            name: "license",
-            message: `What license will you be attributing to this project?`,
-            type: 'list',
-            choices: [
-                {
-                    name: 'GNU AGPL v3',
-                    value: 'agpl3'
-                },
-                {
-                    name: 'GNU GPL v3',
-                    value: 'gpl3'
-                },
-                {
-                    name: 'GNU LGPL v3',
-                    value: 'lgpl3'
-                },
-                {
-                    name: 'Mozilla Public License 2.0',
-                    value: 'mpl2'
-                },
-                {
-                    name: 'Apache License 2.0',
-                    value: 'al2'
-                },
-                {
-                    name: 'MIT License',
-                    value: 'mit'
-                },
-                {
-                    name: 'Boost Software License 1.0',
-                    value: 'boost1'
-                },
-                {
-                    name: 'The Unlicense',
-                    value: 'unlicense'
-                }
-            ]
-        }
-    ];
-
     static socials = [
         {
             name: "email",
@@ -169,17 +169,15 @@ class Questions {
 
     static async ask() {
         console.log(Questions.message.welcome);
+
         const requiredAnswers = await inquirer.prompt(Questions.required);
         const sectionAnswers = await inquirer.prompt(Questions.sections);
+
         let socialAnswers;
         if (sectionAnswers.sections.includes("socials")) {
             socialAnswers = await inquirer.prompt(Questions.socials);
         }
-        console.log(requiredAnswers);
-        console.log(sectionAnswers);
-        if (socialAnswers) {
-            console.log(socialAnswers);
-        }
+        
     }
 }
 
